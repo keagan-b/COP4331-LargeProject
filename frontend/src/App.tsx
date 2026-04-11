e39dd05d076441de48b5812550f0329855c020ab
Jamothey <148384485+Jamothey@users.noreply.github.com>
2026-04-11 19:00:12 -0400

    Change route path from '/' to '/home'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/category/:categoryId" element={<CategoryPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
