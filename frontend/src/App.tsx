534a0471ad624402682491ad24fc741cc8721748
Jamothey <148384485+Jamothey@users.noreply.github.com>
2026-04-11 19:10:22 -0400

    Add PrivateRoute for protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
@@ -6,16 +6,22 @@ import CategoryPage from "./pages/CategoryPage";
 import CollectionPage from "./pages/CollectionPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 
+const PrivateRoute = ({ element }: { element: JSX.Element }) => {
+  const token = localStorage.getItem("token");
+  return token ? element : <Navigate to="/login" replace />;
+};
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/category/:categoryId" element={<CategoryPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/home" element={<PrivateRoute element={<HomePage />} />} />
+        <Route path="/category/:categoryId" element={<PrivateRoute element={<CategoryPage />} />} />
+        <Route path="/collection/:collectionId" element={<PrivateRoute element={<CollectionPage />} />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/resetpassword" element={<ResetPasswordPage />} />
-        <Route path="/collection/:collectionId" element={<CollectionPage />} />
       </Routes>
     </BrowserRouter>
   );
