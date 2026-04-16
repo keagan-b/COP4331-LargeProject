ce3a12df38973fa96106d0e398179715eaafbfaf
Rahi1518 <rahip1518@gmail.com>
2026-04-15 21:38:57 -0400

    Update Login.tsx for accessibility

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -168,10 +168,10 @@ const Login: React.FC = () => {
       <div className="login-box">
         <h2>Login</h2>
         <form onSubmit={handleSubmit}>
-          <label>Email</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-          <label>Password</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           <Link to="/resetpassword" className="forgot-password">Forgot Password</Link>
           <button type="submit">Log In</button>
         </form>
